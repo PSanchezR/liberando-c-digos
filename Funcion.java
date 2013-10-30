811a908f7666eca5118d215d9dbe2df7e15eeae4
PSanchezR <pablosanchez@correo.ugr.es>
2013-10-30 17:47:06 +0100

    Create Funcion.java
    Clase función utilizada en DesencriptadoPolialfabetico.

---
+++ Funcion.java
@@ -0,0 +1,33 @@
+/*
+<<copyright 2013 Pablo Sánchez Robles>>
+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
+package divide_cadena;
+
+/**
+ *
+ * @author pablosanchezrobles
+ *
+ * Esta clase es solo una estructura de datos para almacenar los objetos
+ * de tipo funcion utilizados por la clase principal(DesencriptadoPolialfabetico)
+ */
+public class Funcion {
+    public Funcion(){}
+    public int frec[];
+    public int pos =0;
+    public String funcion="";
+    public String alfabeto="";
+    public boolean contada= false;
+    public String desencriptada="";
+}
